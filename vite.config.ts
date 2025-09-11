d6d85b430a58c4ad611150e9b3a082f693f9dfef
ci010 <cijhn@hotmail.com>
2025-09-11 12:59:59 +0800

    update base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  // Ensure correct asset paths when served from https://voxelum.github.io/xmcl-log-viewer/
+  base: '/xmcl-log-viewer/',
 })
